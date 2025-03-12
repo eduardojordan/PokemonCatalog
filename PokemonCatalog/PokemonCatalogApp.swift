6ccb601ce31a2695679d9c3b3f13e3729346ac8f
Eduardo Jordan <eduardojordan@hotmail.com>
2025-03-12 11:05:24 -0400

    initial commit

---
+++ PokemonCatalog/PokemonCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCatalogApp.swift
+//  PokemonCatalog
+//
+//  Created by Eduardo Jordan on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
